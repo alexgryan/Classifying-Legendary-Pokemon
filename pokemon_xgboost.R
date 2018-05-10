d210671414615b773d2be2709031829705ac58c1
Alex Ryan <alexryan1995@gmail.com>
2018-05-09 17:43:37 -0700

    Added Visualizations
    -Created two visualizations for exploratory analysis.
    -Need to create more relevant features based off the visualizations now

--- pokemon_xgboost.R
+++ pokemon_xgboost.R
@@ -1,5 +1,10 @@
 library(caret)
 library(doSNOW)
+library(magrittr)
+library(dplyr)
+library(ggthemes)
+library(fmsb)
+
 #Loading Data
 train = read.csv("Pokemon.csv", stringsAsFactors = FALSE)
 View(train)
@@ -36,6 +41,41 @@ train$hasHighSpDef <- as.factor(train$hasHighSpDef)
 train$hasHighSpeed <- as.factor(train$hasHighSpeed)
 
 View(train)
+
+#Visualization for Legendaries and Non-Legendaries by Type
+pokemon<-read.csv("Pokemon.csv",sep=",",stringsAsFactors=F)
+colnames(pokemon)<-c("id","Name","Type.1","Type.2", "Total", "HP","Attack","Defense","Sp.Atk","Sp.Def","Speed","Generation","Legendary")
+Type.1<-c("Dragon","Steel","Flying","Psychic","Rock" ,"Fire","Electric" ,"Dark","Ghost" ,"Ground","Ice", "Water","Grass","Fighting", "Fairy" ,"Poison","Normal","Bug")
+color<-c("#6F35FC","#B7B7CE","#A98FF3","#F95587","#B6A136","#EE8130","#F7D02C","#705746","#735797","#E2BF65","#96D9D6","#6390F0","#7AC74C","#C22E28","#D685AD","#A33EA1","#A8A77A","#A6B91A")
+COL<-data.frame(Type.1,color)
+
+merge(
+  merge(pokemon %>% dplyr::group_by(Type.1) %>% dplyr::summarize(tot=n()),
+        pokemon %>% dplyr::group_by(Type.1,Legendary) %>% dplyr::summarize(count=n()),by='Type.1'),
+  COL, by='Type.1') %>% 
+  ggplot(aes(x=reorder(Type.1,tot),y=count)) + 
+  geom_bar(aes(fill=color,alpha=Legendary),color='white',size=.25,stat='identity') + 
+  scale_fill_identity() + coord_flip() + theme_fivethirtyeight() + 
+  ggtitle("Pokemon Distribution") + scale_alpha_discrete(range=c(.9,.6))
+
+res<-data.frame(pokemon %>% dplyr::select(Type.1,HP, Attack, Defense, Sp.Atk, Sp.Def, Speed) %>% dplyr::group_by(Type.1) %>% dplyr::summarise_all(funs(mean)) %>% mutate(sumChars = HP + Attack + Defense + Sp.Atk + Sp.Def + Speed) %>% arrange(-sumChars))
+res$color<-color
+max<- ceiling(apply(res[,2:7], 2, function(x) max(x, na.rm = TRUE)) %>% sapply(as.double)) %>% as.vector
+min<-rep.int(0,6)
+
+par(mfrow=c(3,6))
+par(mar=c(1,1,1,1))
+for(i in 1:nrow(res)){
+  curCol<-(col2rgb(as.character(res$color[i]))%>% as.integer())/255
+  radarchart(rbind(max,min,res[i,2:7]),
+             axistype=2 , 
+             pcol=rgb(curCol[1],curCol[2],curCol[3], alpha = 1) ,
+             pfcol=rgb(curCol[1],curCol[2],curCol[3],.5) ,
+             plwd=2 , cglcol="grey", cglty=1, 
+             axislabcol="black", caxislabels=seq(0,2000,5), cglwd=0.8, vlcex=0.8,
+             title=as.character(res$Type.1[i]))
+}
+
 #Subsets the data we have factored and created features for
 features <- c("Type.1", "Type.2", "Generation", "Legendary","OverallStrength", "isDragon", "hasHighHP", "hasHighDefense","hasHighSpAtk","hasHighSpDef","hasHighSpeed")
 train <- train[, features]
@@ -67,7 +107,7 @@ tune.grid <- expand.grid(eta = c(0.05, 0.075, 0.1),
                          nrounds = c(50, 75, 100),
                          max_depth = 4:8,
                          min_child_weight = c(2.0, 2.25, 2.5),
-                         colsample_bytree = c(0.3, 0.4, 0.5),
+                         colsample_bytree = c(0.5, 0.75, 1.0),
                          gamma = 0,
                          subsample = 1)
 View(tune.grid)
@@ -86,53 +126,8 @@ stopCluster(cl)
 
 caret.cv
 
-#Predict based off tuning paramers
-preds <- predict(caret.cv, pokemon.test)
-confusionMatrix(preds, pokemon.test$Legendary)
-
-#Creating the Training and Test Splits for Dragon Classifier
-set.seed(54234)
-indexes <- createDataPartition(train$isDragon,
-                               times = 1,
-                               p = 0.75,
-                               list = FALSE)
-dragon.train <- train[indexes,]
-dragon.test <- train[-indexes,]
-
-
-# Examine the proportions of the Survived class lable across
-# the datasets.
-prop.table(table(train$isDragon))
-prop.table(table(dragon.train$isDragon))
-prop.table(table(dragon.test$isDragon))
-
-# Cross validation and tuning for model
-train.control <- trainControl(method = "repeatedcv",
-                              number = 10,
-                              repeats = 3,
-                              search = "grid")
-
-tune.grid <- expand.grid(eta = c(0.05, 0.075, 0.1),
-                         nrounds = c(50, 75, 100),
-                         max_depth = 4:8,
-                         min_child_weight = c(2.0, 2.25, 2.5),
-                         colsample_bytree = c(0.3, 0.4, 0.5),
-                         gamma = 0,
-                         subsample = 1)
-
-#Creating Cluster
-cl <- makeCluster(3, type = "SOCK")
-registerDoSNOW(cl)
-
-#Gather training model with 2 clusters to identify legendary pokemon
-caret.cv <- train(isDragon ~ ., 
-                  data = dragon.train,
-                  method = "xgbTree",
-                  tuneGrid = tune.grid,
-                  trControl = train.control)
-stopCluster(cl)
-
-caret.cv
+xgbHat = predict(caret.cv, pokemon.test)
+confusionMatrix(xgbHat, pokemon.test$Legendary)
 
 #Naive Bayes Classifier
 library(e1071)
